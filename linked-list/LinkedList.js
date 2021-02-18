1fea2005d1fe4f0a3251713770cfbf28214b8c6d
cpbingham <cbingham96@gmail.com>
2021-02-17 22:36:52 -0500

    Barebones LinkedList

---
+++ linked-list/LinkedList.js
@@ -0,0 +1,21 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }    
+}
+
+class LinkedList {
+    constructor(head = null) {
+        this.head = head;
+    }
+
+}
+
+let node1 = new Node(2);
+let node2 = new Node(4);
+node1.next = node2;
+
+let list = new LinkedList(node1);
+
+console.log(list.head.next.data);
