e13723f86c2593c027bd99a72aa425052b1504c6
cpbingham <cbingham96@gmail.com>
2021-02-18 21:03:12 -0500

    Adding basic opertaions

--- linked-list/LinkedList.js
+++ linked-list/LinkedList.js
@@ -10,6 +10,46 @@ class LinkedList {
         this.head = head;
     }
 
+    printList() {
+        let x = this.head;
+        while (x) {
+            console.log(x.data);
+            x = x.next;
+        }
+    }
+
+    search(data) {
+        let x = this.head;
+        while (x !== null && x.data !== data) {
+            x = x.next;
+        }
+        return x;
+    }
+
+    insert(newNode) {
+        newNode.next = this.head;
+        this.head = newNode;
+    }
+
+    delete(data) {
+        let x = this.head;
+        if (x.data === data) {
+            this.head = x.next;
+            return;
+        }
+
+        let prev;
+        while (x !== null && x.data !== data) {
+            prev = x;
+            x = x.next;
+        }
+
+        if (x === null) {
+            return;
+        }
+
+        prev.next = x.next;
+    }
 }
 
 let node1 = new Node(2);
@@ -18,4 +58,15 @@ node1.next = node2;
 
 let list = new LinkedList(node1);
 
-console.log(list.head.next.data);
+list.printList();
+console.log(list.search(4));
+console.log(list.search(3));
+
+let node3 = new Node(11);
+list.insert(node3);
+list.printList();
+
+console.log('')
+
+list.delete(2);
+list.printList();
